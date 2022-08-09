33a6c298c9fa5c12b526ca79b8839585ad9849ba
quintin <554389585@qq.com>
2022-08-09 22:21:14 +0800

    快排优化，使用随机的分区点

--- src/模版/排序/QuickSort.java
+++ src/模版/排序/QuickSort.java
@@ -5,7 +5,8 @@ import java.util.Random;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] a = {5, 2, 3, 1};
+        int[] a = {5, 1, 1, 2, 0, 0};
+        System.out.println("arrays:" + Arrays.toString(a));
         QuickSort quickSort = new QuickSort();
         quickSort.sort(a);
         System.out.println(Arrays.toString(a));
@@ -22,19 +23,17 @@ public class QuickSort {
      */
     public void sort(int[] a, int p, int r) {
         if (p >= r) {
-            System.out.printf("p >= r, p:%s, r:%s, return \n", p, r);
-            System.out.println();
             return;
         }
         // 分区点
-        System.out.printf("原地分区前，arr:%s \n", Arrays.toString(a));
+//        System.out.printf("原地分区前，arr:%s \n", Arrays.toString(a));
         int q = partition(a, p, r);
-        System.out.printf("原地分区后，分区点的下标:%s, 分区点的值:%s, arr:%s \n", q, a[q], Arrays.toString(a));
-        System.out.println();
+//        System.out.printf("原地分区后，分区点的下标:%s, 分区点的值:%s, arr:%s \n", q, a[q], Arrays.toString(a));
+//        System.out.println();
 
-        System.out.printf("排序左侧 =======>，%s 到 %s \n", p, q - 1);
+//        System.out.printf("排序左侧 =======> %s 到 %s \n", p, q - 1);
         sort(a, p, q - 1);
-        System.out.printf("排序右侧 <=======，%s 到 %s \n", q + 1, r);
+//        System.out.printf("排序右侧 <======= %s 到 %s \n", q + 1, r);
         sort(a, q + 1, r);
     }
 
@@ -47,51 +46,39 @@ public class QuickSort {
      * @return
      */
     public int partition(int[] arr, int p, int r) {
-        // 获取分区点
-//        int randomPivot = getRandomPivot(p, r);
-        int randomPivot = 1;
-        int pivot = arr[randomPivot];
-        System.out.printf("分区点的下标为：%s, 分区点的值为：%s \n", randomPivot, pivot);
+        // 获取分区点下标
+        int randomPivot = getRandomPivot(p, r);
+        swap(arr, r, randomPivot);
+        // 取 l 到 r 最后一个元素做为分区点
+        int pivot = arr[r];
 
         // i 是大于分区点的第一个元素的下标
         int i = p;
         for (int j = p; j < r; j++) {
             // 相等也要交换，否则类似于 {3,3,3,3} 这样的数组下标就都是 0 不会往前
             if (arr[j] <= pivot) {
-                System.out.printf("%s <= %s, swap, %s\n", arr[j], pivot, Arrays.toString(arr));
                 // 交换下标为 i 和 j 的元素的值
                 if (i != j) {
-                    System.out.printf("交换 %s 和 %s 的值\n", arr[i], arr[j]);
                     swap(arr, i, j);
-                    System.out.println("arr:" + Arrays.toString(arr));
                 }
                 i++;
-            } else {
-                System.out.printf("%s > %s, no swap, %s\n", arr[i], pivot, Arrays.toString(arr));
             }
         }
         /**
-         * 交换下标为 i 和 pivot 的元素的值
-         * arr[pivot] 是分区点 pivot 的值
+         * 交换下标为 i 和 r 的元素的值
+         * arr[r] 是分区点 pivot
          * arr[i] 是大于分区点的第一个值
          */
-//        if (i != randomPivot) {
-//            System.out.printf("j >= r, 交换下标为 %s 和 %s 的元素的值\n", i, randomPivot);
-//            swap(arr, i, randomPivot);
-//        }
+        if (i != r) {
+            swap(arr, i, r);
+        }
         return i;
     }
 
     public int getRandomPivot(int start, int end) {
         //创建Random类对象
         Random random = new Random();
-
-        //产生随机数
-        int number = random.nextInt(end - start + 1) + start;
-
-        //打印随机数
-        System.out.println("产生一个 " + start + " 到 " + end + " 之间的随机整数：" + number);
-        return number;
+        return random.nextInt(end - start + 1) + start;
     }
 
     /**
@@ -101,7 +88,7 @@ public class QuickSort {
      * @param i
      * @param j
      */
-    private static void swap(int[] arr, int i, int j) {
+    private void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
